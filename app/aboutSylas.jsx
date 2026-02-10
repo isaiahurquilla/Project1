474643a19b41b09feeddd9f2b6463352cbc67756
CrumyBoi <sylassanchez@gmail.com>
2026-02-10 15:36:34 -0800

    Added Sylas About page and Skills/Projects page

---
+++ app/aboutSylas.jsx
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import{ Link, link } from 'expo-router'
+
+const aboutapp = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={('../assets/favicon.png')} style={{ width: 100, height: 100 }} />
+
+      <Text style={styles.title}>
+        Hey, I'm Sylas Sanchez, a student at California State University, Bakersfield. I'm currently pursuing a degree in Computer Science. 
+      </Text>
+      <Text style={styles.regularText}>
+        I'm very passionate about technology and programming, I've wanted to be a progammer since I was a kid 
+        and this app I've worked on along side with Isaiah is a small taste of what I'm capable of.
+      </Text>
+      <Text style={styles.regularText}>
+        Some of my personal interests aside from programming include Video Games and Economics.
+      </Text>
+      <Text style={styles.regularText}>
+        Video Games are a massive part of my life and are most defintely the reason why I got into programming in the first place.
+      </Text>
+      <Text style={styles.regularText}>
+        I'm still not entirely sure what I want to do with my Computer Science degree, but I'm hoping to find a job in the tech industry after I graduate
+      </Text>
+      <Text style={styles.regularText}>
+        Some Subjects I'm interested in are Software Development and possibly Cyber Security though I'm not entirely sold on it yet
+      </Text>
+      <Text style={styles.regularText}>
+        Fun Fact about me is that I built my own desktop computer when I was 13 years old; I still use it today all the time and I've slowly upgrading its parts over the years.
+      </Text>
+      <Link href="/sylasSP" style={styles.linkButton}> Skills / Project ideas</Link>
+      <Link href="https://github.com/ssanchez124" style={styles.linkButton}> View My GitHub Profile</Link>
+      <Link href="/" style={styles.linkButton}> Back to Home Page</Link>
+    </View>
+  )
+}
+
+export default aboutapp
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff7b7bff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  linkButton: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+    boxShadow: ' 4px 6px rgba(0, 0, 0, 0.1)',
+  },
+  regularText: {
+    fontSize: 16,
+    fontWeight: "400",
+    marginTop: 10, 
+    marginBottom: 10
+  }
+})
