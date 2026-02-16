ba6ccfd5ff551b08f39d9beb1e65ffbe3b242544
Isaiah <isaiahurquilla@gmail.com>
2026-02-15 18:21:18 -0800

    LinkButton

--- app/LinkButton.jsx
+++ app/LinkButton.jsx
@@ -1,3 +1,6 @@
+//This file is used to create a reusable LinkButton component that can be used throughout the app to navigate between different pages. 
+// It takes in two props: href, which is the destination URL, and children, which is the text that will be displayed on the button. 
+// The component uses the Link component from expo-router to handle navigation and applies some styling to make it look like a button.
 import { StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
 import React from 'react'
